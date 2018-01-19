b76610fbe050f9a93f7bbe65c69ab95e4cf8a6ad
PetejStone <petejstoney@gmail.com>
2018-01-18 18:33:32 -0800

    Added overlay

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ let cards = document.querySelector('.cards');
 let card = document.querySelectorAll('.card');
 
 
-
-
-
 $.ajax({
   url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',
@@ -16,19 +13,14 @@ $.ajax({
       thumbnail += '<img src="'+results[i].picture.thumbnail+'"></a>';
       card[i].innerHTML = thumbnail;
     }
-
-
-
-
-
   }  //end of function data
 }); //End of Ajax
 
+const image = document.getElementsByClassName('image');
+const overlay = document.getElementById('overlay');
 
-
-
- //End of function(datasuccess: function(data) {
-
-  //
-  // success: function(data) {
-  //   members = data.results;
+for (let i=0; i<image.length; i+=1) {
+  image[i].addEventListener('click', ()=> {
+    overlay.style.display = 'flex';
+  })
+}
