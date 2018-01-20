8f0daec4f6e19aa34e4b7cc0cce8abd253b81659
PetejStone <petejstoney@gmail.com>
2018-01-19 17:24:13 -0800

    Added contact and birthday info
    Converted DOB into en-us

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 let cards = document.querySelector('.cards');
 let card = document.querySelectorAll('.card');
+let cardOverlay = document.querySelectorAll('.card--overlay');
 
 
 $.ajax({
-  url: 'https://randomuser.me/api/?results=12',
+  url: 'https://randomuser.me/api?results=12&nat=us',
   dataType: 'json',
   success: function (data) {
     let results = data.results;
@@ -15,14 +16,42 @@ $.ajax({
       card[i].innerHTML = thumbnail;
     }
     //End of add user image function
+
+    //Adds user info (before overlay)
     for (let i=0; i < card.length; i+=1) {
-      // let thumbnail = '<a href="'+results[i].picture.large+'" class="image">';
-      let userInfo = '<div class="user--info"><p class="user user--name">'+results[i].name.first+'</p>';
+      let userInfo = '<div class="user--info"><p class="user user--name">'+results[i].name.first +' '+ results[i].name.last+'</p>';
       userInfo += '<p class="user user--email">'+results[i].email+'</p>';
-      userInfo += '<p class="user user--city">'+results[i].location.city+'</p>';
+      userInfo += '<p class="user user--city">'+results[i].location.city+'</p><div>';
       card[i].insertAdjacentHTML('beforeend', userInfo);
     }
-    //Adds user info (before overlay)
+    //End user info (before overlay)
+
+
+    //Adds user image (overlay)
+    for (let i=0; i < cardOverlay.length; i+=1) {
+      let overlayImage = '<img src="'+results[i].picture.large+'" class="large"></a>';
+      cardOverlay[i].innerHTML = overlayImage;
+    }
+    //End of user image (overlay)
+
+    //Adds user info (overlay)
+    for (let i=0; i < cardOverlay.length; i+=1) {
+      let userInfo = '<div class="overlay--info"><p class="user user--name">'+results[i].name.first+' '+results[i].name.last+'</p>';
+      userInfo += '<p class="user user--email">'+results[i].email+'</p>';
+      userInfo += '<p class="user user--city">'+results[i].location.city+'</p></div>';
+      cardOverlay[i].insertAdjacentHTML('beforeend', userInfo);
+    }
+    //End of user info (overlay)
+
+    //User contact (overlay)
+    for (let i=0; i < cardOverlay.length; i+=1) {
+      let userInfo = '<div class="overlay--contact"><p class="user user--phone">'+results[i].phone+'</p>';
+      userInfo += '<p class="user user--address">'+results[i].location.street+' '+results[i].location.city+', ' +results[i].location.state+' '+results[i].location.postcode+ '</p>';
+      userInfo += '<p class="dob">Birthday: '+new Date(results[i].dob).toLocaleDateString('en-US')+'</p></div>';
+      cardOverlay[i].insertAdjacentHTML('beforeend', userInfo);
+    }
+    //End user contact (overlay)
+
 
 
   }  //end of function data
